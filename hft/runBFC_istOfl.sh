709e510d7c9590ece6ae28b617910ecef8b12f5a
ypwang <ypwang>
2014-03-11 17:46:21 +0000

    update IST offline chain shell

--- hft/runBFC_istOfl.sh
+++ hft/runBFC_istOfl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 starver dev
 
-root4star -b -q -l 'bfc.C(1,5000,"in,istRaw,istDb,istCluster,istHit,istQA","st_physics_15016035_raw_1000001.daq")' > log_istOfl
+root4star -b -q -l 'bfc.C(1,5000,"in,ry2014,istRaw,istDb,istCluster,istHit,istQA","st_physics_15016035_raw_1000001.daq")' > log_istOfl
 
 
 Nonsense
