7fb70011b96efc11361a71f1efdd6ec03c3146d1
Dmitri Smirnov <d.s@plexoos.com>
2014-03-11 18:48:01 +0000

    Local 2014 geometry definitions

---
+++ hft/StarDb/Geometry/pxl/pxlOnPst.y2014.C
@@ -0,0 +1,13 @@
+TDataSet *CreateTable() {
+// -----------------------------------------------------------------
+// PxlPxlOnPst Allocated rows: 1  Used rows: 1
+//  Table: Survey_st[0]
+// ====================================================================
+// ------  Test whether this table share library was loaded ------
+
+  if (!gROOT->GetClass("St_Survey")) return 0;
+  Survey_st row = {1, 1,0,0, 0,1,0, 0,0,1, 0,0,0,.1,.1,.1,.1,.1,.1,"ideal position"};
+  St_Survey *tableSet = new St_Survey("pxlOnPst",1);
+  tableSet->AddAt(&row.Id, 0);
+  return (TDataSet *)tableSet;
+}
