dd71110307cad4af04a84ca4030f02e7ad761c7e
Dmitri Smirnov <d.s@plexoos.com>
2014-05-17 15:39:29 +0000

    Updated PXL sensor dimensions

--- hft/StRoot/StHftPool/EventT/EventT.cxx
+++ hft/StRoot/StHftPool/EventT/EventT.cxx
@@ -180,10 +180,10 @@ Int_t  EventT::Build(StEvent *stEvent, UInt_t MinNoHits, Double_t pCut, StMaker 
 
   UInt_t nT = 0;
 
-  const double IST_Width_X = 3.8016;
-  const double IST_Width_Z = 7.53;
-  const double PXL_Width_X = 2.07;
-  const double PXL_Width_Z = 2.07;
+  const double IST_Width_X = 3.8016; // cm
+  const double IST_Width_Z = 7.53;   // cm
+  const double PXL_Width_X = 1.9210; // cm
+  const double PXL_Width_Z = 1.9872; // cm
 
 
   // Load hits - PXL
