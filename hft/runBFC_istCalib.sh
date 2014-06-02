7a67d7a9156c61b9ac5e2f38475f91846b2405c2
ypwang <ypwang>
2014-06-02 15:59:50 +0000

    scripts update for IST offline/calibration chain

--- hft/runBFC_istCalib.sh
+++ hft/runBFC_istCalib.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 starver dev
 
-root4star -b -q -l 'bfc.C(1,5000,"in,istRaw,istDb,istCalib","st_physics_15016035_raw_1000001.daq")' > log_istCalib
+rm istPedNoiseTable.dat
+root4star -b -q -l 'bfc_calib.C(1,200,"in,istRaw,istDb,istCalib","/star/data03/daq/2014/075/15075079/st_physics_adc_15075079_raw_0500002.daq")'
+root4star -b -q -l 'bfc_calib.C(1,1000,"in,istRaw,istDb,istCalib","/star/data03/daq/2014/075/15075079/st_physics_adc_15075079_raw_0500002.daq")'
--- hft/runBFC_istOfl.sh
+++ hft/runBFC_istOfl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 starver dev
 
-root4star -b -q -l 'bfc.C(1,5000,"in,ry2014,istRaw,istDb,istCluster,istHit,istQA","st_physics_15016035_raw_1000001.daq")' > log_istOfl
+root4star -b -q -l 'bfc.C(1,200,"in,ry2014,istRaw,istDb,istCluster,istHit,istQA","/star/data03/daq/2014/093/15093017/st_physics_15093017_raw_1000001.daq")'
 
