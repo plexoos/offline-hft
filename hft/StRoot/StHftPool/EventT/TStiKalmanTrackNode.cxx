8dfe844291f9e6e1c517205aa215b3e930338c84
Dmitri Smirnov <d.s@plexoos.com>
2014-06-03 11:48:34 +0000

    Do not inherit from StiKalmanTrackNode; Added new constructors and overloaded operators

--- hft/StRoot/StHftPool/EventT/TStiKalmanTrackNode.cxx
+++ hft/StRoot/StHftPool/EventT/TStiKalmanTrackNode.cxx
@@ -1,5 +1,59 @@
 
 #include "StHftPool/EventT/TStiKalmanTrackNode.h"
 
+#include "Sti/StiPlacement.h"
+
 
 ClassImp(TStiKalmanTrackNode)
+
+
+TStiKalmanTrackNode::TStiKalmanTrackNode() : TObject(),
+   fEnergyLosses(-1), fLayerRadius(0), fStiDetectorName()
+{
+}
+
+
+TStiKalmanTrackNode::TStiKalmanTrackNode(const StiKalmanTrackNode &stiKTN) : TObject(),
+   fEnergyLosses(-1), fLayerRadius(0), fStiDetectorName()
+{
+   Info("TStiKalmanTrackNode", "Convert StiKalmanTrackNode to TStiKalmanTrackNode");
+
+   fEnergyLosses = stiKTN.getEnergyLosses();
+
+   if (stiKTN.getDetector()) {
+      Info("TStiKalmanTrackNode", "detector found: %s", stiKTN.getDetector()->getName().c_str());
+      fStiDetectorName = stiKTN.getDetector()->getName();
+      Info("TStiKalmanTrackNode", "detector found: %s", fStiDetectorName.c_str());
+
+      StiPlacement* stiPlacement = stiKTN.getDetector()->getPlacement();
+      assert(stiPlacement);
+      fLayerRadius = stiPlacement->getLayerRadius();
+
+   } else {
+      Info("TStiKalmanTrackNode", "detector not found:" );
+   }
+}
+
+
+TStiKalmanTrackNode & TStiKalmanTrackNode::operator=(const StiKalmanTrackNode &stiKTN)
+{
+   TStiKalmanTrackNode tmpNode(stiKTN);
+
+   fStiDetectorName = tmpNode.fStiDetectorName;
+
+   return *this;
+}
+
+
+void TStiKalmanTrackNode::Print(Option_t *opt) const
+{
+   printf("fEnergyLosses:    %f\n", fEnergyLosses);
+   printf("fLayerRadius:     %f\n", fLayerRadius);
+   printf("fStiDetectorName: %s\n", fStiDetectorName.c_str());
+}
+
+
+bool operator< (const TStiKalmanTrackNode& lhs, const TStiKalmanTrackNode& rhs)
+{
+   return lhs.fLayerRadius < rhs.fLayerRadius;
+}
--- hft/StRoot/StHftPool/EventT/TStiKalmanTrackNode.h
+++ hft/StRoot/StHftPool/EventT/TStiKalmanTrackNode.h
@@ -2,16 +2,31 @@
 #define TStiKalmanTrackNode_h
 
 #include "TObject.h"
-#include "Sti/StiKalmanTrack.h"
+#include "Sti/StiKalmanTrackNode.h"
 
 
-class TStiKalmanTrackNode : public TObject, private StiKalmanTrackNode
+class TStiKalmanTrackNode : public TObject
 {
+public:
+
+   TStiKalmanTrackNode();
+   TStiKalmanTrackNode(const StiKalmanTrackNode &stiKTN);
+   TStiKalmanTrackNode & operator=(const StiKalmanTrackNode &stiKTN);
+   virtual void Print(Option_t *opt = "") const;
+
+   friend bool operator< (const TStiKalmanTrackNode& lhs, const TStiKalmanTrackNode& rhs);
+
 protected:
 
-   using StiKalmanTrackNode::mEnergyLosses; //!
+   double      fEnergyLosses;
+   double      fLayerRadius;
+   std::string fStiDetectorName;
 
    ClassDef(TStiKalmanTrackNode, 1)
 };
+
+
+bool operator< (const TStiKalmanTrackNode& lhs, const TStiKalmanTrackNode& rhs);
+
 
 #endif
