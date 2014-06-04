f589b0e65d3b6c8b4108bad11f90562be8b3d8c4
Dmitri Smirnov <d.s@plexoos.com>
2014-06-04 19:27:11 +0000

    Add two basic histograms to fill with track and energy loss information

--- hft/StRoot/StHftPool/StiLajaTester/StiLajaRootFile.cxx
+++ hft/StRoot/StHftPool/StiLajaTester/StiLajaRootFile.cxx
@@ -1,5 +1,9 @@
 #include "StiLajaRootFile.h"
 
+#include "TH2S.h"
+#include "TProfile2D.h"
+#include "TROOT.h"
+
 
 StiLajaRootFile::StiLajaRootFile() : TFile(), mHs()
 {
@@ -18,19 +22,38 @@ StiLajaRootFile::StiLajaRootFile(const char *fname, Option_t *option, const char
 
 StiLajaRootFile::~StiLajaRootFile()
 {
-   while (!mHs.empty()) delete mHs.end()->second, mHs.erase(mHs.end());
+   while (!mHs.empty()) {
+      delete mHs.begin()->second;
+      mHs.erase(mHs.begin());
+   }
 }
 
 
 void StiLajaRootFile::BookHists()
 {
-   mHs["hVolumeMatchedTracks"] = new TH2S("hVolumeMatchedTracks", "hVolumeMatchedTracks", 10, -1, 1, 10, -M_PI, M_PI);
+   // Create the root histograms outside of the current root file, i.e. they will not be associated with the file
+   // TDirectory and won't be deleted on TFile::Close()
+   gROOT->cd();
+
+   mHs["hTrackCountVsEtaVsPhi"] = new TH2S("hTrackCountVsEtaVsPhi", "hTrackCountVsEtaVsPhi", 11, -1, 1, 11, -M_PI, M_PI);
+   mHs["hTotalELossVsEtaVsPhi"] = new TProfile2D("hTotalELossVsEtaVsPhi", "hTotalELossVsEtaVsPhi", 11, -1, 1, 11, -M_PI, M_PI);
 }
 
 
-void StiLajaRootFile::FillHists()
+void StiLajaRootFile::FillHists(EventT &eventT)
 {
-   //mHs["hVolumeMatchedTracks"]->Fill();
+   std::vector<TStiKalmanTrack>::const_iterator iTStiKTrack = eventT.fTStiKalmanTracks.begin();
+
+   for ( ; iTStiKTrack != eventT.fTStiKalmanTracks.end(); ++iTStiKTrack) {
+      //iTStiKTrack->Print();
+      const TStiKalmanTrack &kTrack = *iTStiKTrack;
+
+      // Take the first node with the smallest radius
+      const TStiKalmanTrackNode& dcaNode = kTrack.GetDcaNode();
+
+      mHs["hTrackCountVsEtaVsPhi"]->Fill(dcaNode.GetTrackP().Eta(), dcaNode.GetTrackP().Phi());
+      ((TProfile2D*) mHs["hTotalELossVsEtaVsPhi"])->Fill(dcaNode.GetTrackP().Eta(), dcaNode.GetTrackP().Phi(), kTrack.GetEnergyLosses(), 1);
+   }
 }
 
 
--- hft/StRoot/StHftPool/StiLajaTester/StiLajaRootFile.h
+++ hft/StRoot/StHftPool/StiLajaTester/StiLajaRootFile.h
@@ -7,6 +7,8 @@
 #include "TFile.h"
 #include "StMaker.h"
 
+#include "StHftPool/EventT/EventT.h"
+
 
 typedef std::map<std::string, TH1*>                    HistMap;
 typedef std::map<std::string, TH1*>::iterator          HistMapIter;
@@ -21,11 +23,12 @@ public:
    StiLajaRootFile(const char* fname, Option_t* option="", const char* ftitle="", Int_t compress=1);
    ~StiLajaRootFile();
 
-   void BookHists();
-   void FillHists();
+   void FillHists(EventT &eventT);
    void Close(Option_t *option="");
 
  private:
+
+   void BookHists();
 
    HistMap mHs;
 
