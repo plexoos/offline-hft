5b04e06579d710e72415ff87e1010e83859b3963
Dmitri Smirnov <d.s@plexoos.com>
2014-06-18 22:24:55 +0000

    Added new submission scripts and correposonding templates for condor batch system. The sctipts can be used to produce simulation *.fz files and then hftree trees

---
+++ hft/tests/submit_jobs_hftree.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Requires at least one argument:
+#
+# submit_jobs_hftree.sh RUN_FILE_LIST
+#
+# RUN_FILE_LIST is the full path to a text file with a list of *.fz files
+#
+# Usage example:
+#
+# submit_jobs_hftree.sh ../path/to/filelist_fz.txt
+#
+
+RUN_FILE_LIST=$1
+
+BFC_OPTIONS='tpcRS y2014a AgML MakeEvent ITTF pxlFastSim PxlIT pxlDb istDb istFastSim IstIT Idst BAna l0 Tree logger Sti tpcDB TpcHitMover TpxClu bbcSim btofsim tags emcY2 EEfs evout -dstout IdTruth geantout big fzin MiniMcMk clearmem HftMatTree'
+
+# Replace all spaces with underscores
+BFC_OPTIONS=`echo ${BFC_OPTIONS[*]} | sed 's/ /_/g'`
+
+INP_DIR=$OFFLINE_HFT_RESULTS_DIR/z_scan_3
+OUT_DIR=$OFFLINE_HFT_RESULTS_DIR/z_scan_hftree_3
+
+echo
+echo RUN_FILE_LIST   = $RUN_FILE_LIST
+echo OFFLINE_HFT_DIR = $OFFLINE_HFT_DIR
+echo INP_DIR         = $INP_DIR
+echo OUT_DIR         = $OUT_DIR
+echo STAR_VERSION    = $STAR_VERSION
+echo BFC_OPTIONS     = $BFC_OPTIONS
+echo LOG_DIR         = $OUT_DIR
+echo
+
+for JOB_RUN_FILE_NAME in `cat $RUN_FILE_LIST`
+do
+   echo
+   echo "Submitting job for JOB_RUN_FILE_NAME =" $JOB_RUN_FILE_NAME
+   star-submit-template -template $OFFLINE_HFT_DIR/offline/hft/tests/job_template.xml \
+      -entities INP_DIR=$INP_DIR,OUT_DIR=$OUT_DIR,CODE_DIR=$OFFLINE_HFT_DIR,JOB_RUN_FILE_NAME=$JOB_RUN_FILE_NAME,STAR_VERSION=$STAR_VERSION,STAR_HOST_SYS=$STAR_HOST_SYS,BFC_OPTIONS=$BFC_OPTIONS,LOG_DIR=$OUT_DIR
+   echo
+done
---
+++ hft/tests/submit_jobs_stiscan_zslice.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+OUT_DIR=$OFFLINE_HFT_RESULTS_DIR
+
+echo
+echo STAR_VERSION    = $STAR_VERSION
+echo OFFLINE_HFT_DIR = $OFFLINE_HFT_DIR
+echo OUT_DIR         = $OUT_DIR
+echo LOG_DIR         = $OUT_DIR
+echo
+
+for STISCAN_Z in {-30..30}
+do
+   JOB_RUN_FILE_NAME=$(printf "stiscan_z_%03d" $STISCAN_Z)
+   echo
+   echo "Submitting job for JOB_RUN_FILE_NAME: " $JOB_RUN_FILE_NAME
+   echo "Submitting job for STISCAN_Z: " $STISCAN_Z
+   star-submit-template -template $OFFLINE_HFT_DIR/offline/hft/tests/job_template_stiscan_zslice.xml \
+      -entities OUT_DIR=$OUT_DIR,CODE_DIR=$OFFLINE_HFT_DIR,STISCAN_Z=$STISCAN_Z,JOB_RUN_FILE_NAME=$JOB_RUN_FILE_NAME,STAR_VERSION=$STAR_VERSION,STAR_HOST_SYS=$STAR_HOST_SYS,LOG_DIR=$OUT_DIR
+   echo
+done
