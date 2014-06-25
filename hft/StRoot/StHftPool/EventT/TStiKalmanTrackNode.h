817ef2738e9688ffd5a4a959f27564c1d66ba941
Dmitri Smirnov <d.s@plexoos.com>
2014-06-25 21:51:49 +0000

    TStiKalmanTrackNode: Changed type of data members from double to float since the class is persistent

--- hft/StRoot/StHftPool/EventT/TStiKalmanTrackNode.h
+++ hft/StRoot/StHftPool/EventT/TStiKalmanTrackNode.h
@@ -19,8 +19,8 @@ public:
    TStiKalmanTrackNode & operator=(const StiKalmanTrackNode &stiKTN);
    const TVector3& GetTrackP() const { return fTrackP; }
    const TVector3& GetPosition() const { return fPosition; }
-   double GetEnergyLosses() const { return fEnergyLosses; }
-   double GetNodeRadius() const { return fNodeRadius; }
+   float GetEnergyLosses() const { return fEnergyLosses; }
+   float GetNodeRadius() const { return fNodeRadius; }
    float GetNodeCenterRefAngle() const { return fNodeCenterRefAngle; }
    std::string GetVolumeName() const { return fVolumeName; }
    bool   MatchedVolName(const std::string & pattern) const;
@@ -33,10 +33,10 @@ protected:
 
    TVector3    fPosition;          ///< Coordinates of the track state/node position
    TVector3    fTrackP;            ///< Track momentum vector in global CS
-   double      fEnergyLosses;      ///< Energy lost in the volume
-   double      fNodeRadius;        ///< The nominal radius of the Sti volume associated with this node
-   std::string fVolumeName;
+   float       fEnergyLosses;      ///< Energy lost in the volume
+   float       fNodeRadius;        ///< The nominal radius of the Sti volume associated with this node
    float       fNodeCenterRefAngle;   ///< Angle to the center of the Sti volume associated with this node
+   std::string fVolumeName;           ///< Name of Sti volume
 
    ClassDef(TStiKalmanTrackNode, 1)
 };
