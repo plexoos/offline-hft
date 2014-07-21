7634a7a2995470a1059d04c436ab0d858b4201fb
Dmitri Smirnov <d.s@plexoos.com>
2014-07-21 18:31:51 -0400

    Added a common container that can be used to fill the same set of histograms using different trees

---
+++ hft/StRoot/StHftPool/EventT/StiScanHistContainer.h
@@ -0,0 +1,43 @@
+#ifndef StiScanHistContainer_h
+#define StiScanHistContainer_h
+
+#include <map>
+#include <set>
+#include <string>
+
+#include "TDirectoryFile.h"
+#include "TH2S.h"
+#include "StMaker.h"
+
+#include "StHftPool/EventT/EventT.h"
+
+
+typedef std::map<std::string, TH1*>                    HistMap;
+typedef std::map<std::string, TH1*>::iterator          HistMapIter;
+typedef std::map<std::string, TH1*>::const_iterator    HistMapConstIter;
+
+
+class StiScanHistContainer : public TDirectoryFile
+{
+public:
+
+   StiScanHistContainer();
+   StiScanHistContainer(const char* name, const char* title, Option_t* option = "", TDirectory* motherDir = 0);
+   ~StiScanHistContainer();
+
+   void FillHists(const EventT &eventT, const std::set<std::string> *volumeList=0);
+   void SaveAllAs();
+
+ private:
+
+   void BookHists();
+   void FillHists(const TStiKalmanTrack &kalmTrack, const std::set<std::string> *volumeList=0);
+
+   HistMap mHs;
+   double  mNodeZMin;
+   double  mNodeZMax;
+
+   ClassDef(StiScanHistContainer, 1)
+};
+
+#endif
