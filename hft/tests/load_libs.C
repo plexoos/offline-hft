1d836e0ade9d3e81b0925204289adf2d2e926984
Dmitri Smirnov <d.s@plexoos.com>
2014-07-22 14:47:42 +0000

    Added ROOT macro to load common star libraries from the official location and modified ones from the working area

---
+++ hft/tests/load_libs.C
@@ -0,0 +1,27 @@
+{
+   gROOT->Reset();
+   gROOT->Clear();
+
+   gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+   loadSharedLibraries();
+
+   gSystem->Load("$STAR_LIB/libSti");
+   gSystem->Load("$STAR_LIB/libStiUtilities");
+   gSystem->Load("$STAR_LIB/libStTpcDb");
+   gSystem->Load("$STAR_LIB/libStSsdDbMaker");
+   gSystem->Load("$STAR_LIB/libStDbUtilities");
+   gSystem->Load("$STAR_LIB/libStDetectorDbMaker");
+   gSystem->Load("$STAR_LIB/libStSvtDbMaker");
+   gSystem->Load("$STAR_LIB/libStiMaker");
+   gSystem->Load("$STAR_LIB/libStiPxl");
+
+   // Extra root libraries
+   gSystem->Load("$ROOTSYS/lib/libEve");
+   gSystem->Load("$ROOTSYS/lib/libGeom");
+   gSystem->Load("$ROOTSYS/lib/libTable");
+
+   // temp
+   gSystem->Load("$OFFLINE_HFT_DIR/.$STAR_HOST_SYS/lib/libStIstDbMaker");
+   gSystem->Load("$OFFLINE_HFT_DIR/.$STAR_HOST_SYS/lib/libStHftPoolEventT");
+   gSystem->Load("$OFFLINE_HFT_DIR/.$STAR_HOST_SYS/lib/libStHftPoolHftMatchedTree");
+}
