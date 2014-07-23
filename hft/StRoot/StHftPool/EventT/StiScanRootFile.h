6a2cb0e6c32f44099afab04a9ce886bcc6594cb5
Dmitri Smirnov <d.s@plexoos.com>
2014-07-23 10:59:32 -0400

    StiScanRootFile: Two histogram containers added to the ROOT file. Identical sets of histograms for sti and geant track propagation studies

--- hft/StRoot/StHftPool/EventT/StiScanRootFile.h
+++ hft/StRoot/StHftPool/EventT/StiScanRootFile.h
@@ -6,12 +6,16 @@
 #include <string>
 
 #include "TH2S.h"
+#include "TDirectoryFile.h"
 #include "TFile.h"
 #include "StMaker.h"
 
 #include "StHftPool/EventT/EventT.h"
 
 
+typedef std::map<std::string, TDirectoryFile*>                   TDirMap;
+typedef std::map<std::string, TDirectoryFile*>::iterator         TDirMapIter;
+typedef std::map<std::string, TDirectoryFile*>::const_iterator   TDirMapConstIter;
 
 
 class StiScanRootFile : public TFile
@@ -30,6 +34,7 @@ public:
 
    void BookHists();
 
+   TDirMap mDirs;   ///< A string-to-TDirectoryFile map for convenient access to enclosed directories
 
    ClassDef(StiScanRootFile, 1)
 };
