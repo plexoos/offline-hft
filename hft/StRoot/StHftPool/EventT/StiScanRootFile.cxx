5f292c40215f298a036eb2c8f00ada6985154b4f
Dmitri Smirnov <d.s@plexoos.com>
2014-07-23 20:29:22 +0000

    StiScanRootFile: Two histogram containers added to the ROOT file. Identical sets of histograms for sti and geant track propagation studies

--- hft/StRoot/StHftPool/EventT/StiScanRootFile.cxx
+++ hft/StRoot/StHftPool/EventT/StiScanRootFile.cxx
@@ -7,15 +7,16 @@
 #include "TProfile2D.h"
 #include "TROOT.h"
 
+#include "StHftPool/EventT/StiScanHistContainer.h"
 
-StiScanRootFile::StiScanRootFile() : TFile()
+StiScanRootFile::StiScanRootFile() : TFile(), mDirs()
 {
    BookHists();
 }
 
 
 StiScanRootFile::StiScanRootFile(const char *fname, Option_t *option, const char *ftitle, Int_t compress) :
-   TFile(fname, option, ftitle, compress)
+   TFile(fname, option, ftitle, compress), mDirs()
 {
    printf("Created ROOT file: %s\n", GetName());
 
@@ -25,6 +26,8 @@ StiScanRootFile::StiScanRootFile(const char *fname, Option_t *option, const char
 
 void StiScanRootFile::BookHists()
 {
+   mDirs["sti"] = new StiScanHistContainer("sti", "sti", "", this);
+   mDirs["geant"] = new StiScanHistContainer("geant", "geant", "", this);
 }
 
 
@@ -47,9 +50,16 @@ void StiScanRootFile::Close(Option_t *option)
 
 void StiScanRootFile::SaveAllAs()
 {
+   for (TDirMapConstIter iDir=mDirs.begin() ; iDir!=mDirs.end(); ++iDir)
+   {
+      std::string  dirName = iDir->first;
+      StiScanHistContainer *container = static_cast<StiScanHistContainer*> (iDir->second);
 
+      if (!container) {
+         Error("SaveAllAs", "No container/directory found for key %s. Skipping...", dirName.c_str());
          continue;
       }
 
+      container->SaveAllAs();
    }
 }
