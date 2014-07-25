45c123c142c0f99a9fc01526b6818aad362abcf3
bouchet <bouchet>
2014-07-25 15:15:29 +0000

    initial version

---
+++ hft/StRoot/StSsdDbMaker/StSsdLinkDef.h
@@ -0,0 +1,20 @@
+// $Id$
+//
+// $Log$
+// Revision 1.1  2014/07/25 15:15:29  bouchet
+// initial version
+//
+// Revision 1.2  2005/06/20 14:21:38  lmartin
+// CVS tags added
+//
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class StSsdDbMaker;
+#pragma link C++ class StSstDbMaker;
+#pragma link C++ global gStSsdDbMaker;
+#pragma link C++ global gStSstDbMaker;
+
+#endif
