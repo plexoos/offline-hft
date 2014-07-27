1d5cc185ad98437ff4bc199614c6209cd402ed3a
Dmitri Smirnov <d.s@plexoos.com>
2014-07-27 20:22:44 +0000

    Modified condor submission script to accept the path to a job template as a parameter

--- hft/tests/submit_jobs_stiscan_zslice.sh
+++ hft/tests/submit_jobs_stiscan_zslice.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+JOB_TEMPLATE_XML=$1
+
 OUT_DIR=$OFFLINE_HFT_RESULTS_DIR
 
 echo
@@ -15,7 +17,7 @@ do
    echo
    echo "Submitting job for JOB_RUN_FILE_NAME: " $JOB_RUN_FILE_NAME
    echo "Submitting job for STISCAN_Z: " $STISCAN_Z
-   star-submit-template -template $OFFLINE_HFT_DIR/offline/hft/tests/job_template_stiscan_zslice.xml \
+   star-submit-template -template $JOB_TEMPLATE_XML \
       -entities OUT_DIR=$OUT_DIR,CODE_DIR=$OFFLINE_HFT_DIR,STISCAN_Z=$STISCAN_Z,JOB_RUN_FILE_NAME=$JOB_RUN_FILE_NAME,STAR_VERSION=$STAR_VERSION,STAR_HOST_SYS=$STAR_HOST_SYS,LOG_DIR=$OUT_DIR
    echo
 done
