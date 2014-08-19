546cacaaaa09e811510e75819ca932577116ca84
Dmitri Smirnov <d.s@plexoos.com>
2014-08-19 21:01:57 +0000

    Squashed commits modifying stiscan:
    - Added a function to build a map mapping volume path to its hash value
    - Added an auxilary function to build a TGeoManager from a ROOT file
    - Create a hash map matching a volume path to its hash, and assign it to a static member of AgUStep to be used by geant events for hash decoding
    - Pass the list of user volume patterns to the histogram container so that, histograms can be filled with data corresponding to these volumes
    - Delete EvenG object after use

--- tests/stiscan.cc
+++ tests/stiscan.cc
@@ -1,8 +1,11 @@
 #include <fstream>
 #include <string>
+#include <unordered_map>
 
 #include "TChain.h"
 #include "TError.h"
+#include "TGeoNavigator.h"
+#include "TGeoManager.h"
 #include "TRandom.h"
 #include "TROOT.h"
 
@@ -12,14 +15,19 @@
 #include "StHftPool/EventT/StiScanRootFile.h"
 
 typedef Event EventG;
+typedef std::unordered_map<size_t, std::string> Hash2StringMap;
 
 
 // These globals are not used but required since we link against TGiant3.o
 int    Margc=0;
 char** Margv=NULL;
 
+TGeoManager *gGeoManager = 0;
+
 
 void loop_hftree(StiScanPrgOptions &poProc);
+void make_geometry(std::string geoTag="y2014a");
+void create_volume_hash_map(TGeoNavigator &geoNav, Hash2StringMap &hash2PathMap);
 
 
 int main(int argc, char **argv)
@@ -29,6 +37,18 @@ int main(int argc, char **argv)
 
    StiScanPrgOptions poProc(argc, argv, hftTreeName, geantStepTreeName);
    poProc.ProcessOptions();
+
+   // Initialize gGeoManager with geometry from a ROOT file
+   make_geometry("y2014a");
+
+   gGeoManager->cd("HALL_1/CAVE_1/TpcRefSys_1/IDSM_1");
+   TGeoNavigator* geoNav = gGeoManager->GetCurrentNavigator();
+   assert(geoNav);
+
+   Hash2StringMap hash2PathMap;
+   create_volume_hash_map(*geoNav, hash2PathMap);
+
+   AgUStep::fgHash2PathMap = &hash2PathMap;
 
    loop_hftree(poProc);
 
@@ -86,11 +106,12 @@ void loop_hftree(StiScanPrgOptions &poProc)
 
       if (poProc.DoGeantStepTree()) {
          geantStepChain->GetEntry(iEvent-1);
-         outRootFile.FillHists(*eventG);
+         outRootFile.FillHists(*eventG, &poProc.GetVolumeList());
       }
    }
 
    delete eventT;
+   delete eventG;
 
    outRootFile.FillDerivedHists();
 
@@ -101,4 +122,72 @@ void loop_hftree(StiScanPrgOptions &poProc)
 
    outRootFile.Write();
    outRootFile.Close();
+}
+
+
+void make_geometry(std::string geoTag)
+{
+   std::string myGeomTag("y2014a");
+
+   TGeoManager::Import( Form("%s.root", myGeomTag.c_str()) );
+
+   if (!gGeoManager) {
+      Error("make_geometry", "No gGeoManager found");
+      exit(EXIT_FAILURE);
+   }
+}
+
+
+void create_volume_hash_map(TGeoNavigator &geoNav, Hash2StringMap &hash2PathMap)
+{
+   TGeoNode* currNode = geoNav.GetCurrentNode();
+
+   if (!currNode) {
+      Warning("create_volume_hash_map", "Invalid TGeoNode provided as input. Skipping...");
+      return;
+   }
+
+   // Keep track of current level with respect to the original node
+   static int level = 0;
+
+   std::string currentPath( geoNav.GetPath() );
+
+   int nDaughters = currNode->GetVolume()->GetNdaughters();
+
+   if (nDaughters) {
+
+      TGeoNode* motherNode = currNode;
+      for (int iDaughter = 0; iDaughter < nDaughters; iDaughter++)
+      {
+         TGeoNode *daughter = motherNode->GetVolume()->GetNode(iDaughter);
+
+         geoNav.CdDown(daughter);
+         level++;
+
+         create_volume_hash_map(geoNav, hash2PathMap);
+      }
+
+   } else { // We are here if this node is a leaf, i.e. no daughters
+
+      // Add this volume to the hash map
+      std::string hashedPath(currentPath);
+
+      // Remove TpcRefSys_1/ substring as it not relevant for geometry trees used in simulation 
+      size_t first_pos = hashedPath.find("TpcRefSys_1/");
+      hashedPath.replace(first_pos, std::string("TpcRefSys_1/").length(), "");
+
+      std::hash<std::string> hash_fn;
+      std::size_t hash_value = hash_fn(hashedPath);
+
+      std::pair<size_t, std::string> hash2Path(hash_value, hashedPath);
+
+      hash2PathMap.insert(hash2Path);
+
+      geoNav.cd(currentPath.c_str());
+   }
+
+   if (level > 0) {
+      geoNav.CdUp();
+      level--;
+   }
 }
