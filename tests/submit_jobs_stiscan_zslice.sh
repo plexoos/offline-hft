dded6be23abf63cb7708f97080343c5b3b02ca44
Dmitri Smirnov <d.s@plexoos.com>
2014-09-05 16:23:19 -0400

    Do not use a plus sign in the file names

--- tests/submit_jobs_stiscan_zslice.sh
+++ tests/submit_jobs_stiscan_zslice.sh
@@ -13,7 +13,7 @@ echo
 
 for STISCAN_Z in {-30..30}
 do
-   JOB_RUN_FILE_NAME=$(printf "stiscan_zslice_%+04d" $STISCAN_Z)
+   JOB_RUN_FILE_NAME=$(printf "stiscan_zslice_%04d" $STISCAN_Z)
    echo
    echo "Submitting job for JOB_RUN_FILE_NAME: " $JOB_RUN_FILE_NAME
    echo "Submitting job for STISCAN_Z: " $STISCAN_Z
