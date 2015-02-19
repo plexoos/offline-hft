e0b8ef411b32b987b4c302c37f8999eed01cda98
Dmitri Smirnov <d.s@plexoos.com>
2015-02-19 01:09:32 +0000

    Changed data member access to private
    Proper priviligies can be set later when/if it is decided to inherit from this class

--- StRoot/StIstFastSimMaker/StIstFastSimMaker.h
+++ StRoot/StIstFastSimMaker/StIstFastSimMaker.h
@@ -1,4 +1,4 @@
-/* $Id: StIstFastSimMaker.h,v 1.18 2015/02/19 01:09:21 smirnovd Exp $ */
+/* $Id: StIstFastSimMaker.h,v 1.19 2015/02/19 01:09:32 smirnovd Exp $ */
 
 #ifndef StIstFastSimMaker_h
 #define StIstFastSimMaker_h
@@ -36,11 +36,12 @@ public:
    virtual void  Clear(Option_t *option="");
 
    virtual const char *GetCVS() const {
-      static const char cvs[] = "Tag $Name:  $ $Id: StIstFastSimMaker.h,v 1.18 2015/02/19 01:09:21 smirnovd Exp $ built "__DATE__" "__TIME__ ;
+      static const char cvs[] = "Tag $Name:  $ $Id: StIstFastSimMaker.h,v 1.19 2015/02/19 01:09:32 smirnovd Exp $ built "__DATE__" "__TIME__ ;
       return cvs;
    }
 
-protected:
+private:
+
    THashList *mIstRot;
    StIstDb *mIstDb;
    Bool_t mBuildIdealGeom; ///< Switch between ideal and misaligned geometries. Default is true (ideal)
@@ -50,8 +51,6 @@ protected:
    Double_t mResXIst1;
    Double_t mResZIst1;
    Bool_t mSmear; ///< Smear generated IST hit positions. Default is true
-
-private:
 
    /// Routine to smear hit by resolution with gaussian, mean zero and width res
    Double_t distortHit(const Double_t x, const Double_t res, const Double_t detLength);
